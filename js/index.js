022352337b07a83772e6c257fd93c3e54ace611d
hayhay23 <hmuramatsu23@punahou.edu>
2016-06-29 14:24:10 -1000

    challenge2

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var random = [
+"1",
+"2",
+"3",
+"4",
+"5",
+]
+
+var count = 0;
+
+var cycle = function(array) {
+
+	random =Math.floor(Math.random()*random.length)
+	if () {}
+
+	// console.log(array[count]); 
+	// if (count == array.length-1) {
+	// 	count=0;
+	// } else {
+	// 	count++;
+	// };
+};
